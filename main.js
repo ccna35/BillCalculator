c44a46878e46c696a9ae34e832f46abe6f627b68
ccna35 <shawkykhalil@gmail.com>
2021-09-16 21:58:39 +0200

    Fgghh

---
+++ main.js
@@ -0,0 +1,18 @@
+let container = document.querySelector('.container');
+let bill = document.querySelector('#bill');
+let slider = document.querySelector('#slider');
+let tip = document.querySelector('#tip');
+let tip_dollars = document.querySelector('#tipInDollars');
+let total = document.querySelector('#total');
+
+
+function update(){
+  let tip_value = parseFloat(slider.value)/100;
+  tip.textContent = `${slider.value}%`;
+  tip_dollars.textContent = `$${tip_value*parseFloat(bill.value)}`;
+  if (bill.value != '') {
+    total.textContent = `$${tip_value*parseFloat(bill.value)+parseFloat(bill.value)}`
+  }
+}
+
+container.addEventListener('input', update);
