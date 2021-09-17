1d61d4a5f19ad4ce893a5407cf830cd5666580f1
ccna35 <shawkykhalil@gmail.com>
2021-09-17 15:02:56 +0200

    Fghh

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ let tip = document.querySelector('#tip');
 let tip_dollars = document.querySelector('#tipInDollars');
 let total = document.querySelector('#total');
 
-if (bill.value != parseFloat())
-function update(){
-  let tip_value = parseFloat(slider.value)/100;
-  tip.textContent = `${slider.value}%`;
-  tip_dollars.textContent = `$${tip_value*parseFloat(bill.value)}`;
-  if (bill.value != '') {
-    total.textContent = `$${tip_value*parseFloat(bill.value)+parseFloat(bill.value)}`
+if (bill.value != parseFloat('') {
+  function update() {
+    let tip_value = parseFloat(slider.value) / 100;
+    tip.textContent = `${slider.value}%`;
+    tip_dollars.textContent = `$${tip_value*parseFloat(bill.value)}`;
+    if (bill.value != '') {
+      total.textContent = `$${tip_value*parseFloat(bill.value)+parseFloat(bill.value)}`
+    }
   }
 }
 container.addEventListener('input', update);
